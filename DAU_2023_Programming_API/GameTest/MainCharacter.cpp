e0e3c5132cc010ddcf8b01eabca7008ed810084b
Roxn <60963951+roxane-be@users.noreply.github.com>
2023-10-31 14:58:28 +0100

    create class Main Character
    move code sprite for mainCharacter

---
+++ DAU_2023_Programming_API/GameTest/MainCharacter.cpp
@@ -0,0 +1,93 @@
+#include "stdafx.h"
+#include "MainCharacter.h"
+#include "App/app.h"
+#include "App/SimpleSprite.h"
+
+MainCharacter::MainCharacter()
+{
+
+}
+
+void MainCharacter::InitSprite()
+{
+	//------------------------------------------------------------------------
+		// Example Sprite Code....
+	testSprite = App::CreateSprite(".\\TestData\\Test.bmp", 8, 4);
+	testSprite->SetPosition(400.0f, 400.0f);
+	float speed = 1.0f / 15.0f;
+	testSprite->CreateAnimation(ANIM_BACKWARDS, speed, { 0,1,2,3,4,5,6,7 });
+	testSprite->CreateAnimation(ANIM_LEFT, speed, { 8,9,10,11,12,13,14,15 });
+	testSprite->CreateAnimation(ANIM_RIGHT, speed, { 16,17,18,19,20,21,22,23 });
+	testSprite->CreateAnimation(ANIM_FORWARDS, speed, { 24,25,26,27,28,29,30,31 });
+	testSprite->SetScale(1.0f);
+	//------------------------------------------------------------------------
+
+}
+
+void MainCharacter::UpdateSprite(float deltaTime)
+{
+	//------------------------------------------------------------------------
+	// Example Sprite Code....
+	testSprite->Update(deltaTime);
+	if (App::GetController().GetLeftThumbStickX() > 0.5f)
+	{
+		testSprite->SetAnimation(ANIM_RIGHT);
+		float x, y;
+		testSprite->GetPosition(x, y);
+		x += 1.0f;
+		testSprite->SetPosition(x, y);
+	}
+	if (App::GetController().GetLeftThumbStickX() < -0.5f)
+	{
+		testSprite->SetAnimation(ANIM_LEFT);
+		float x, y;
+		testSprite->GetPosition(x, y);
+		x -= 1.0f;
+		testSprite->SetPosition(x, y);
+	}
+	if (App::GetController().GetLeftThumbStickY() > 0.5f)
+	{
+		testSprite->SetAnimation(ANIM_FORWARDS);
+		float x, y;
+		testSprite->GetPosition(x, y);
+		y += 1.0f;
+		testSprite->SetPosition(x, y);
+	}
+	if (App::GetController().GetLeftThumbStickY() < -0.5f)
+	{
+		testSprite->SetAnimation(ANIM_BACKWARDS);
+		float x, y;
+		testSprite->GetPosition(x, y);
+		y -= 1.0f;
+		testSprite->SetPosition(x, y);
+	}
+	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_UP, false))
+	{
+		testSprite->SetScale(testSprite->GetScale() + 0.1f);
+	}
+	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_DOWN, false))
+	{
+		testSprite->SetScale(testSprite->GetScale() - 0.1f);
+	}
+	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_LEFT, false))
+	{
+		testSprite->SetAngle(testSprite->GetAngle() + 0.1f);
+	}
+	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_RIGHT, false))
+	{
+		testSprite->SetAngle(testSprite->GetAngle() - 0.1f);
+	}
+	if (App::GetController().CheckButton(XINPUT_GAMEPAD_A, true))
+	{
+		testSprite->SetAnimation(-1);
+	}
+}
+
+void MainCharacter::RenderSprite()
+{
+	//------------------------------------------------------------------------
+	// Example Sprite Code....
+	testSprite->Draw();
+	//------------------------------------------------------------------------
+
+}
---
+++ DAU_2023_Programming_API/GameTest/MainCharacter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+
+class CSimpleSprite;
+enum AnimationSprite
+{
+	ANIM_FORWARDS,
+	ANIM_BACKWARDS,
+	ANIM_LEFT,
+	ANIM_RIGHT
+};
+
+class MainCharacter
+{
+public:
+	MainCharacter();
+	~MainCharacter() { delete testSprite; };
+
+	//SPRITE
+	void InitSprite();
+	void UpdateSprite(float deltaTime);
+	void RenderSprite();
+
+	//SPRITE
+	CSimpleSprite* testSprite;
+
+};
+
